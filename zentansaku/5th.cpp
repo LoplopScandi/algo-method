2e30fd57bf31c8064c35992875d534840d24b10f
Loplop <sd_lplp@outlook.jp>
2022-10-20 22:58:34 +0900

    add 5th

--- zentansaku/5th.cpp
+++ zentansaku/5th.cpp
@@ -1,1 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+int main(){
+    int N;
+    cin >> N ;
+    vector<int> A(N);
+    for(int i; i < N; i++) cin >> A[i];
+
+    int count = 0;
+    for (int i=0; i<N; ++i) {
+
+        if (A[i]>A[i-1]&&i>0) {
+
+            count+=1;
+
+        }
+
+    }
+        cout << count << endl;
+
+}
